c53581b6bdca5cbe482a95968f8b71dc117d21bd
TrilhaX <141366739+TrilhaX@users.noreply.github.com>
2023-08-11 20:02:35 -0300

    Add files via upload

---
+++ PM.lua
@@ -0,0 +1,229 @@
+if game.PlaceId == 10202329527 then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Trilha Hub | UPDATE 1 + Murcielago 🦇🔥 [PM]", HidePremium = false, IntroText = "Trilha Hub", SaveConfig = true, ConfigFolder = "Trilha Hub"})
+    
+
+    function autoFarmVl()
+        while getgenv().autoFarmVl == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-280.730011, -47.0518761, 247.803207, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+    function autoEat()
+        while getgenv().autoEat == true do
+            wait(1)
+            game:GetService('VirtualInputManager'):SendKeyEvent(true,'E',false,game)
+            wait(1)
+        end
+    end
+
+    function autoFarmS()
+        while getgenv().autoFarmS == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(222.4487, -48.5280838, 226.796432, -0.438529491, 0, -0.898716748, 0, 1, 0, 0.898716748, 0, -0.438529491)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(1)
+        end
+    end
+
+
+    function autoFarmAd()
+        while getgenv().autoFarmAd == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new( -753.917725, 95.3764572, -138.017731, -0.995896995, -0.0100058969, -0.0899401903, -0.00791962538, 0.999691904, -0.0235232376, 0.0901478603, -0.0227144267, -0.995669305)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+
+    function autoFarmT()
+        while getgenv().autoFarmT == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90.6706543, 14.1982489, 1153.12744, 0.368992269, 0, 0.929432452, 0, 1, 0, -0.929432452, 0, 0.368992269)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+    function autoFarmA()
+        while getgenv().autoFarmA == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-189.811752, -35.6681137, -364.355225, 0.0812901855, 3.64456305e-08, 0.996690452, 3.87408896e-08, 1, -3.97263591e-08, -0.996690452, 4.18420392e-08, 0.0812901855)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+
+    function autoFarmM()
+        while getgenv().autoFarmM == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(    -422.538239, 38.5832405, 746.692444, -0.726746142, 2.04836716e-08, -0.686906159, 4.3980414e-08, 1, -1.67110503e-08, 0.686906159, -4.23551079e-08, -0.726746142)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+    
+    function autoFarmH()
+        while getgenv().autoFarmH == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(360.83197, -43.417614, -465.174469, -0.720263124, 0, -0.693701506, 0, 1, 0, 0.693701506, 0, -0.720263124)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+
+    function autoFarmTO()
+        while getgenv().autoFarmTO == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(    -32.0152626, 85.8325653, -865.956604, 0.866527975, -6.58805703e-08, -0.499128491, 1.18402621e-08, 1, -1.11435533e-07, 0.499128491, 9.06521933e-08, 0.866527975)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+
+    function autoFarmAr()
+        while getgenv().autoFarmAr == true do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(72.8447342, 17.3323326, 1151.01038, -0.999772906, 1.23141355e-08, -0.0213109255, 1.27243895e-08, 1, -1.91152854e-08, 0.0213109255, -1.93821137e-08, -0.999772906)
+            wait(0.0000000000000000000000000000000000000000000000000000000000000000001)
+            local ohString1 = "Swing"
+            local ohNumber2 = 1
+            local ohString3 = "Fist"
+
+            game:GetService("ReplicatedStorage").Remotes.Server.Initiate_Server:FireServer(ohString1, ohNumber2, ohString3)
+            wait(0.35)
+        end
+    end
+
+
+
+
+    local Tab = Window:MakeTab({
+        Name = "Main",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+
+    Tab:AddToggle({
+        Name = "Auto Kill Arranca",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoFarmA = Value
+            autoFarmA()
+        end    
+    })
+
+
+    Tab:AddToggle({
+        Name = "Auto Kill Vasto Lorde",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoFarmVl = Value
+            autoFarmVl()
+        end    
+    })
+
+
+
+    Tab:AddToggle({
+        Name = "Auto Kill Adjucha",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoFarmAd = Value
+            autoFarmAd()
+        end    
+    })
+
+
+    Tab:AddToggle({
+        Name = "Auto Kill Hollow",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoFarmH = Value
+            autoFarmH()
+        end    
+    })
+
+
+    Tab:AddToggle({
+        Name = "Auto Eat",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoEat = Value
+            autoEat()
+        end    
+    })
+
+    local Tab = Window:MakeTab({
+        Name = "Auto Boss",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+
+    Tab:AddToggle({
+        Name = "Auto Kill Stark(Event)",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoFarmS = Value
+            autoFarmS()
+        end    
+    })
+
+    Tab:AddToggle({
+        Name = "Auto Kill Menos",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autoFarmM = Value
+            autoFarmM()
+        end    
+    })
+
+
+
+
+    for i,v in pairs(getconnections(game.Players.LocalPlayer.Idled)) do
+        v:Disable()
+    end
+    end
+    OrionLib:Init()   
