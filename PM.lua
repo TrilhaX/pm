fbe54920ad2d8a8bf6f5153246dc0cdaca3892dd
João Vitor Trilha Richartz <141366739+TrilhaX@users.noreply.github.com>
2024-04-28 16:58:45 -0300

    Update PM.lua

--- PM.lua
+++ PM.lua
@@ -1,3 +1,124 @@
+warn('[TEMPEST HUB] Loading Ui')
+wait(1)
+local repo = 'https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
+local Window = Library:CreateWindow({
+
+    Title = 'Tempest Hub | Project Mugetsu',
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
+})
+
+Library:Notify('Loading Project Mugetsu Script', 5)
+
+
+local function GetCFrame(obj)
+    local cframe = CFrame.new()
+    local objType = typeof(obj)
+    
+    if objType == "Vector3" then
+        cframe = CFrame.new(obj)
+    elseif objType == "table" then
+        cframe = CFrame.new(unpack(obj))
+    elseif objType == "string" then
+        local parts = {}
+        for val in obj:gmatch("[^,]+") do
+            table.insert(parts, tonumber(val))
+        end
+        if #parts >= 3 then
+            cframe = CFrame.new(unpack(parts))
+        end
+    elseif objType == "Instance" then
+        if obj:IsA("Model") then
+            local rootPart = obj:FindFirstChild("HumanoidRootPart") or obj:FindFirstChild("Torso")
+            if rootPart then
+                cframe = rootPart.CFrame
+            end
+        elseif obj:IsA("Part") then
+            cframe = obj.CFrame
+        end
+    end
+    
+    return cframe
+end
+
+function RollDeClan()
+    while getgenv().RollDeClan == true do
+        local clan = game:GetService("ReplicatedStorage").Player_Datas.eaenescau0.Slot_1.Clan.Value
+        if selectedClan == clan then
+            print("Pegou UHUUUUL")
+        else 
+            local args = {
+                [1] = 1
+            }
+            
+            game:GetService("ReplicatedStorage"):WaitForChild("Spin"):InvokeServer(unpack(args))            
+        end
+    end
+end
+
+local codes = {
+    "ChangesAgainSorryForShutdown",
+    "USECODESINMAINMENU",
+    "CrispyReasonForBugs",
+    "150kLikes",
+    "QuincyFixes",
+    "AgainSorryForShutdown",
+    "UPDATE2MoreRerolls",
+    "UPDATE2MoreRerolls2",
+    "FirstBalanceChange",
+    "BlackButlerPublicSchoolArc",
+    "TestOrbCode",
+}
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+}
+
+local LeftGroupBox = Tabs.Main:AddLeftGroupbox('Player')
+
+local MyButton = LeftGroupBox:AddButton({
+    Text = 'Reedem Codes',
+    Func = function()
+        local args = {
+            [1] = codes
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("Code"):InvokeServer(unpack(args))        
+    end,
+    DoubleClick = false,
+    Tooltip = ''
+})
+
+LeftGroupBox:AddDropdown('Map', {
+    Values = {"Asano", "Arisawa", "Joaquin", "Haida", "Hanakari", "Honsho", "Ide", "Iijima", "Kanoniji", "Kojima", "Kunieda", "Natsui", "Shigeo", "Dokugamine", "Sado", "Inoue", "Ginjo", "Kutsuzawa", "Tsukishima", "Shishigawara", "Yukio", "Kotetsu", "Mugurama", "Yadomaru", "Iba", "Kira", "Hirako", "Ushoda", "Sarugaki", "Amagi", "Valkyrie", "Jagerjaquez", "Hitsugaya", "Hisagi", "Ichimaru", "Shihoin", "Unohana","Sasuke", "Naruto", "Itachi", "Urahara", "Kurosaki", "Itadori", "Brook", "Ginjo", "Ichibei", "Gojo","Tsukishima"},
+    Default = "None",
+    Multi = false,
+    Text = 'Choose Map',
+    Callback = function(value)
+        selectedClan = value
+    end
+})
+
+LeftGroupBox:AddToggle('AutoRoll', {
+    Text = 'Auto Roll',
+    Default = false,
+    Tooltip = '',
+    Callback = function(Value)
+        getgenv().RollDeClan = Value
+        if Value then
+            RollDeClan()
+        end
+    end
+})
+
+
 local FrameTimer = tick()
 local FrameCounter = 0
 local FPS = 60
